ed734b05de5f6533e6ca8b86b6ae6e5445eb1764
Fatih Solen <50904951+fsolen@users.noreply.github.com>
2024-12-30 00:29:05 +0300

    Update analyze_metrics.py

--- fDRS/analyze_metrics.py
+++ fDRS/analyze_metrics.py
@@ -3,22 +3,29 @@ import sys
 
 def analyze_metrics(vm_metrics, host_metrics):
     action_plan = []
+
     for vm in vm_metrics:
+        optimal_host = None
         for host in host_metrics:
             if (
                 host['cpu_free'] > vm['cpu_usage'] and
                 host['memory_free'] > vm['memory_usage'] and
                 host['network_usage'] < 80 and
                 host['datastore_free'] > vm['disk_usage'] and
-                host['cpu_overcommit'] < 4 and
-                vm['is_vnuma_compatible']
+                host['cpu_overcommit'] < 2 and
+                vm['is_vnuma_compatible'] and
+                host['vm_count'] < host['max_vm_count']
             ):
-                action_plan.append({
-                    "vm_id": vm['vm_id'],
-                    "source_host": vm['host'],
-                    "target_host": host['name']
-                })
-                break
+                if not optimal_host or host['cpu_free'] > optimal_host['cpu_free']:
+                    optimal_host = host
+        
+        if optimal_host:
+            action_plan.append({
+                "vm_id": vm['vm_id'],
+                "source_host": vm['host'],
+                "target_host": optimal_host['name']
+            })
+
     return action_plan
 
 if __name__ == "__main__":
