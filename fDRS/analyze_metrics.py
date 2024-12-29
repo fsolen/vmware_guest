09052f4a74e2ea01b258384467cb0dcab92cbe38
Fatih Solen <50904951+fsolen@users.noreply.github.com>
2024-12-30 00:25:29 +0300

    Create analyze_metrics.py

---
+++ fDRS/analyze_metrics.py
@@ -0,0 +1,34 @@
+import json
+import sys
+
+def analyze_metrics(vm_metrics, host_metrics):
+    action_plan = []
+    for vm in vm_metrics:
+        for host in host_metrics:
+            if (
+                host['cpu_free'] > vm['cpu_usage'] and
+                host['memory_free'] > vm['memory_usage'] and
+                host['network_usage'] < 80 and
+                host['datastore_free'] > vm['disk_usage'] and
+                host['cpu_overcommit'] < 4 and
+                vm['is_vnuma_compatible']
+            ):
+                action_plan.append({
+                    "vm_id": vm['vm_id'],
+                    "source_host": vm['host'],
+                    "target_host": host['name']
+                })
+                break
+    return action_plan
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: analyze_metrics.py <vm_metrics.json> <host_metrics.json>")
+        sys.exit(1)
+
+    with open(sys.argv[1], 'r') as vm_file, open(sys.argv[2], 'r') as host_file:
+        vm_metrics = json.load(vm_file)
+        host_metrics = json.load(host_file)
+
+    plan = analyze_metrics(vm_metrics, host_metrics)
+    print(json.dumps(plan, indent=2))
